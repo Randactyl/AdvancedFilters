1a89a6138f2391b21d0a6b8b42f38c261b4563bd
Randactyl <rafowler@randactyl.com>
2015-04-22 12:00:05 -0700

    update texture map to correspond with master

--- AF_TextureMap.lua
+++ AF_TextureMap.lua
@@ -4,18 +4,77 @@ AF_TextureMap = {
 		downTexture = [[/esoui/art/inventory/inventory_tabicon_all_down.dds]],
 		flash = [[/esoui/art/inventory/inventory_tabicon_all_over.dds]]
 	},
+	TROPHY = {
+		upTexture = "AdvancedFilters/assets/miscellaneous/trophy/trophy_up.dds",
+		downTexture = "AdvancedFilters/assets/miscellaneous/trophy/trophy_down.dds",
+		flash = "AdvancedFilters/assets/miscellaneous/trophy/trophy_over.dds",
+	},
 
-	--consumables
-	REPAIR = {
-		upTexture = "AdvancedFilters/assets/consumables/repair/repair_up.dds",
-		downTexture = "AdvancedFilters/assets/consumables/repair/repair_down.dds",
-		flash = "AdvancedFilters/assets/consumables/repair/repair_over.dds",
+	--WEAPONS
+	ONEHAND = {
+		upTexture = "AdvancedFilters/assets/weapons/onehanded_up.dds",
+		downTexture = "AdvancedFilters/assets/weapons/onehanded_down.dds",
+		flash = "AdvancedFilters/assets/weapons/onehanded_over.dds",
 	},
-	CONTAINER = {
-		upTexture = "AdvancedFilters/assets/consumables/containers/container_up.dds",
-		downTexture = "AdvancedFilters/assets/consumables/containers/container_down.dds",
-		flash = "AdvancedFilters/assets/consumables/containers/container_over.dds",
+	TWOHAND = {
+		upTexture = "AdvancedFilters/assets/weapons/twohanded_up.dds",
+		downTexture = "AdvancedFilters/assets/weapons/twohanded_down.dds",
+		flash = "AdvancedFilters/assets/weapons/twohanded_over.dds",
 	},
+	BOW = {
+		upTexture = "AdvancedFilters/assets/weapons/bow_up.dds",
+		downTexture = "AdvancedFilters/assets/weapons/bow_down.dds",
+		flash = "AdvancedFilters/assets/weapons/bow_over.dds",
+	},
+	DESTRUCTIONSTAFF = {
+		upTexture = "AdvancedFilters/assets/weapons/destruction_up.dds",
+		downTexture = "AdvancedFilters/assets/weapons/destruction_down.dds",
+		flash = "AdvancedFilters/assets/weapons/destruction_over.dds",
+	},
+	HEALSTAFF = {
+		upTexture = "AdvancedFilters/assets/weapons/healing_up.dds",
+		downTexture = "AdvancedFilters/assets/weapons/healing_down.dds",
+		flash = "AdvancedFilters/assets/weapons/healing_over.dds",
+	},
+
+	--ARMOR
+	HEAVY = {
+		upTexture = "/esoui/art/inventory/inventory_tabicon_armor_up.dds",
+		downTexture = "/esoui/art/inventory/inventory_tabicon_armor_down.dds",
+		flash = "/esoui/art/inventory/inventory_tabicon_armor_over.dds",
+	},
+	MEDIUM = {
+		upTexture = "AdvancedFilters/assets/apparel/medium_up.dds",
+		downTexture = "AdvancedFilters/assets/apparel/medium_down.dds",
+		flash = "AdvancedFilters/assets/apparel/medium_over.dds",
+	},
+	LIGHT = {
+		upTexture = "AdvancedFilters/assets/apparel/light_up.dds",
+		downTexture = "AdvancedFilters/assets/apparel/light_down.dds",
+		flash = "AdvancedFilters/assets/apparel/light_over.dds",
+	},
+	SHIELD = {
+		upTexture = "AdvancedFilters/assets/apparel/shield_up.dds",
+		downTexture = "AdvancedFilters/assets/apparel/shield_down.dds",
+		flash = "AdvancedFilters/assets/apparel/shield_over.dds",
+	},
+	JEWELRY = {
+		upTexture = "AdvancedFilters/assets/apparel/jewelry_up.dds",
+		downTexture = "AdvancedFilters/assets/apparel/jewelry_down.dds",
+		flash = "AdvancedFilters/assets/apparel/jewelry_over.dds",
+	},
+	CLOTHING = {
+		upTexture = "AdvancedFilters/assets/materials/style/style_up.dds",
+		downTexture = "AdvancedFilters/assets/materials/style/style_down.dds",
+		flash = "AdvancedFilters/assets/materials/style/style_over.dds",
+	},
+	VANITY = {
+		upTexture = "AdvancedFilters/assets/apparel/vanity_up.dds",
+		downTexture = "AdvancedFilters/assets/apparel/vanity_down.dds",
+		flash = "AdvancedFilters/assets/apparel/vanity_over.dds",
+	},
+
+	--CONSUMABLES
 	FOOD = {
 		upTexture = "AdvancedFilters/assets/consumables/food/food_up.dds",
 		downTexture = "AdvancedFilters/assets/consumables/food/food_down.dds",
@@ -26,38 +85,38 @@ AF_TextureMap = {
 		downTexture = "AdvancedFilters/assets/consumables/drinks/drink_down.dds",
 		flash = "AdvancedFilters/assets/consumables/drinks/drink_over.dds",
 	},
-	POISON = {
-		upTexture = "AdvancedFilters/assets/consumables/poison/poison_up.dds",
-		downTexture = "AdvancedFilters/assets/consumables/poison/poison_down.dds",
-		flash = "AdvancedFilters/assets/consumables/poison/poison_over.dds",
+	RECIPE = {
+		upTexture = "AdvancedFilters/assets/consumables/recipes/recipe_up.dds",
+		downTexture = "AdvancedFilters/assets/consumables/recipes/recipe_down.dds",
+		flash = "AdvancedFilters/assets/consumables/recipes/recipe_over.dds",
 	},
 	POTION = {
 		upTexture = "AdvancedFilters/assets/consumables/potion/potion_up.dds",
 		downTexture = "AdvancedFilters/assets/consumables/potion/potion_down.dds",
 		flash = "AdvancedFilters/assets/consumables/potion/potion_over.dds",
 	},
-	RECIPE = {
-		upTexture = "AdvancedFilters/assets/consumables/recipes/recipe_up.dds",
-		downTexture = "AdvancedFilters/assets/consumables/recipes/recipe_down.dds",
-		flash = "AdvancedFilters/assets/consumables/recipes/recipe_over.dds",
+	POISON = {
+		upTexture = "AdvancedFilters/assets/consumables/poison/poison_up.dds",
+		downTexture = "AdvancedFilters/assets/consumables/poison/poison_down.dds",
+		flash = "AdvancedFilters/assets/consumables/poison/poison_over.dds",
 	},
 	MOTIF = {
 		upTexture = "AdvancedFilters/assets/consumables/motifs/motif_up.dds",
 		downTexture = "AdvancedFilters/assets/consumables/motifs/motif_down.dds",
 		flash = "AdvancedFilters/assets/consumables/motifs/motif_over.dds",
 	},
+	CONTAINER = {
+		upTexture = "AdvancedFilters/assets/consumables/containers/container_up.dds",
+		downTexture = "AdvancedFilters/assets/consumables/containers/container_down.dds",
+		flash = "AdvancedFilters/assets/consumables/containers/container_over.dds",
+	},
+	REPAIR = {
+		upTexture = "AdvancedFilters/assets/consumables/repair/repair_up.dds",
+		downTexture = "AdvancedFilters/assets/consumables/repair/repair_down.dds",
+		flash = "AdvancedFilters/assets/consumables/repair/repair_over.dds",
+	},
 
-	--materials
-	ALCHEMY = {
-		upTexture = "AdvancedFilters/assets/materials/alchemy/alchemy_up.dds",
-		downTexture = "AdvancedFilters/assets/materials/alchemy/alchemy_down.dds",
-		flash = "AdvancedFilters/assets/materials/alchemy/alchemy_over.dds",
-	},
-	ATRAIT = {
-		upTexture = "AdvancedFilters/assets/materials/atrait/atrait_up.dds",
-		downTexture = "AdvancedFilters/assets/materials/atrait/atrait_down.dds",
-		flash = "AdvancedFilters/assets/materials/atrait/atrait_over.dds",
-	},
+	--MATERIALS
 	BLACKSMITHING = {
 		upTexture = "AdvancedFilters/assets/materials/blacksmithing/blacksmithing_up.dds",
 		downTexture = "AdvancedFilters/assets/materials/blacksmithing/blacksmithing_down.dds",
@@ -67,6 +126,16 @@ AF_TextureMap = {
 		upTexture = "AdvancedFilters/assets/materials/clothier/clothier_up.dds",
 		downTexture = "AdvancedFilters/assets/materials/clothier/clothier_down.dds",
 		flash = "AdvancedFilters/assets/materials/clothier/clothier_over.dds",
+	},
+	WOODWORKING = {
+		upTexture = "AdvancedFilters/assets/materials/woodworking/woodworking_up.dds",
+		downTexture = "AdvancedFilters/assets/materials/woodworking/woodworking_down.dds",
+		flash = "AdvancedFilters/assets/materials/woodworking/woodworking_over.dds",
+	},
+	ALCHEMY = {
+		upTexture = "AdvancedFilters/assets/materials/alchemy/alchemy_up.dds",
+		downTexture = "AdvancedFilters/assets/materials/alchemy/alchemy_down.dds",
+		flash = "AdvancedFilters/assets/materials/alchemy/alchemy_over.dds",
 	},
 	ENCHANTING = {
 		upTexture = "AdvancedFilters/assets/materials/enchanting/enchanting_up.dds",
@@ -83,22 +152,27 @@ AF_TextureMap = {
 		downTexture = "AdvancedFilters/assets/materials/style/style_down.dds",
 		flash = "AdvancedFilters/assets/materials/style/style_over.dds",
 	},
-	WOODWORKING = {
-		upTexture = "AdvancedFilters/assets/materials/woodworking/woodworking_up.dds",
-		downTexture = "AdvancedFilters/assets/materials/woodworking/woodworking_down.dds",
-		flash = "AdvancedFilters/assets/materials/woodworking/woodworking_over.dds",
-	},
 	WTRAIT = {
 		upTexture = "AdvancedFilters/assets/materials/wtrait/wtrait_up.dds",
 		downTexture = "AdvancedFilters/assets/materials/wtrait/wtrait_down.dds",
 		flash = "AdvancedFilters/assets/materials/wtrait/wtrait_over.dds",
 	},
-
-	--miscellaneous
-	ARMORGLYPH = {
-		upTexture = "AdvancedFilters/assets/miscellaneous/armorglyph/armorglyph_up.dds",
-		downTexture = "AdvancedFilters/assets/miscellaneous/armorglyph/armorglyph_down.dds",
-		flash = "AdvancedFilters/assets/miscellaneous/armorglyph/armorglyph_over.dds",
+	ATRAIT = {
+		upTexture = "AdvancedFilters/assets/materials/atrait/atrait_up.dds",
+		downTexture = "AdvancedFilters/assets/materials/atrait/atrait_down.dds",
+		flash = "AdvancedFilters/assets/materials/atrait/atrait_over.dds",
+	},
+	
+	--MISCELLANEOUS
+	GLYPHS = {
+		upTexture = "AdvancedFilters/assets/miscellaneous/glyphs/glyphs_up.dds",
+		downTexture = "AdvancedFilters/assets/miscellaneous/glyphs/glyphs_down.dds",
+		flash = "AdvancedFilters/assets/miscellaneous/glyphs/glyphs_over.dds",
+	},
+	SOULGEM = {
+		upTexture = "AdvancedFilters/assets/miscellaneous/soulgem/soulgem_up.dds",
+		downTexture = "AdvancedFilters/assets/miscellaneous/soulgem/soulgem_down.dds",
+		flash = "AdvancedFilters/assets/miscellaneous/soulgem/soulgem_over.dds",
 	},
 	AVAWEAPON = {
 		upTexture = "AdvancedFilters/assets/miscellaneous/avaweapon/avaweapon_up.dds",
@@ -110,39 +184,19 @@ AF_TextureMap = {
 		downTexture = "AdvancedFilters/assets/miscellaneous/bait/bait_down.dds",
 		flash = "AdvancedFilters/assets/miscellaneous/bait/bait_over.dds",
 	},
-	GLYPHS = {
-		upTexture = "AdvancedFilters/assets/miscellaneous/glyphs/glyphs_up.dds",
-		downTexture = "AdvancedFilters/assets/miscellaneous/glyphs/glyphs_down.dds",
-		flash = "AdvancedFilters/assets/miscellaneous/glyphs/glyphs_over.dds",
-	},
-	JEWELRYGLYPH = {
-		upTexture = "AdvancedFilters/assets/miscellaneous/jewelryglyph/jewelryglyph_up.dds",
-		downTexture = "AdvancedFilters/assets/miscellaneous/jewelryglyph/jewelryglyph_down.dds",
-		flash = "AdvancedFilters/assets/miscellaneous/jewelryglyph/jewelryglyph_over.dds",
-	},
-	SOULGEM = {
-		upTexture = "AdvancedFilters/assets/miscellaneous/soulgem/soulgem_up.dds",
-		downTexture = "AdvancedFilters/assets/miscellaneous/soulgem/soulgem_down.dds",
-		flash = "AdvancedFilters/assets/miscellaneous/soulgem/soulgem_over.dds",
-	},
 	TOOL = {
 		upTexture = "AdvancedFilters/assets/consumables/repair/repair_up.dds",
 		downTexture = "AdvancedFilters/assets/consumables/repair/repair_down.dds",
 		flash = "AdvancedFilters/assets/consumables/repair/repair_over.dds",
 	},
+	FENCE = {
+		upTexture = "/esoui/art/vendor/vendor_tabicon_fence_up.dds",
+		downTexture = "/esoui/art/vendor/vendor_tabicon_fence_down.dds",
+		flash = "/esoui/art/vendor/vendor_tabicon_fence_over.dds",
+	},
 	TRASH = {
 		upTexture = "AdvancedFilters/assets/miscellaneous/trash/trash_up.dds",
 		downTexture = "AdvancedFilters/assets/miscellaneous/trash/trash_down.dds",
 		flash = "AdvancedFilters/assets/miscellaneous/trash/trash_over.dds",
-	},
-	TROPHY = {
-		upTexture = "AdvancedFilters/assets/miscellaneous/trophy/trophy_up.dds",
-		downTexture = "AdvancedFilters/assets/miscellaneous/trophy/trophy_down.dds",
-		flash = "AdvancedFilters/assets/miscellaneous/trophy/trophy_over.dds",
-	},
-	WEAPONGLYPH = {
-		upTexture = "AdvancedFilters/assets/miscellaneous/weaponglyph/weaponglyph_up.dds",
-		downTexture = "AdvancedFilters/assets/miscellaneous/weaponglyph/weaponglyph_down.dds",
-		flash = "AdvancedFilters/assets/miscellaneous/weaponglyph/weaponglyph_over.dds",
 	},
 }
