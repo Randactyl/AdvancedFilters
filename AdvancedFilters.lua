775d423bd4aec1c0650dfcc6952b816b6afac568
Randactyl <rafowler@randactyl.com>
2015-07-16 12:44:19 -0700

    update version number for release

--- AdvancedFilters.lua
+++ AdvancedFilters.lua
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------
 --AdvancedFilters.lua
 --Author: ingeniousclown, Randactyl
---v0.8.1.0
+--v0.8.1.1
  
 --Advanced Filters adds a line of subfilters to the inventory
 --screen.
