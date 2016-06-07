026357f92867067f1c63b24d0cfc7385d260d06b
Randactyl <rafowler@randactyl.com>
2016-06-07 09:30:30 -0700

    add new motif extra filter

---
+++ extrafilters/NewMotif.lua
@@ -0,0 +1,46 @@
+local util = AdvancedFilters.util
+
+local function GetFilterCallbackForNewMotif()
+	return function(slot)
+		local itemLink = util.GetItemLink(slot)
+        
+		return util.LibMotifCategories:IsNewMotif(itemLink)
+	end
+end
+
+local dropdownCallback = {
+    [1] = {name = "NewMotif", filterCallback = GetFilterCallbackForNewMotif()},
+}
+
+local strings = {
+    ["NewMotif"] = "New Motif",
+}
+
+local filterInformation = {
+    callbackTable = dropdownCallback,
+    filterType = ITEMFILTERTYPE_WEAPONS,
+    subfilters = {"All",},
+    enStrings = strings,
+}
+
+AdvancedFilters_RegisterFilter(filterInformation)
+
+filterInformation.filterType = ITEMFILTERTYPE_ARMOR
+filterInformation.subfilters = {"Body", "Shield",}
+
+AdvancedFilters_RegisterFilter(filterInformation)
+
+filterInformation.filterType = ITEMFILTERTYPE_CONSUMABLE
+filterInformation.subfilters = {"Motif",}
+
+AdvancedFilters_RegisterFilter(filterInformation)
+
+filterInformation.filterType = ITEMFILTERTYPE_CRAFTING
+filterInformation.subfilters = {"Style",}
+
+AdvancedFilters_RegisterFilter(filterInformation)
+
+filterInformation.filterType = ITEMFILTERTYPE_STYLE_MATERIALS
+filterInformation.subfilters = {"All",}
+
+AdvancedFilters_RegisterFilter(filterInformation)
