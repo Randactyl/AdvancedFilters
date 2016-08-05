fbf18757a82c22cf6259e70cbae095fd6c936f39
Randactyl <rafowler@randactyl.com>
2016-08-05 12:11:30 -0700

    add dropdown filter for tradeable BoP items

---
+++ extrafilters/BoPTradeableFilters.lua
@@ -0,0 +1,39 @@
+local util = AdvancedFilters.util
+
+local function BoPCallback()
+    return function(slot)
+        local bagId, slotIndex = slot.bagId, slot.slotIndex
+
+        if not bagId or not slotIndex then return false end
+
+        if IsItemBoPAndTradeable(bagId, slotIndex) then return true end
+
+        return false
+    end
+end
+
+local dropdownCallback = {
+    {name = "BoPTade", filterCallback = BoPCallback},
+}
+
+local strings = {
+    ["BoPTade"] = "Bound but Tradeable",
+}
+
+local filterInformation = {
+    callbackTable = dropdownCallback,
+    filterType = ITEMFILTERTYPE_WEAPONS,
+    subfilters = {"All",},
+    enStrings = strings,
+}
+
+AdvancedFilters_RegisterFilter(filterInformation)
+
+filterInformation = {
+    callbackTable = dropdownCallback,
+    filterType = ITEMFILTERTYPE_ARMOR,
+    subfilters = {"All",},
+    enStrings = strings,
+}
+
+AdvancedFilters_RegisterFilter(filterInformation)
